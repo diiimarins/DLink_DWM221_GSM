66a0577ea118293b9c4812483b8ab04cd3eb6c8d
diiimarins <51126300+diiimarins@users.noreply.github.com>
2021-11-01 09:48:30 -0300

    Update API.py

--- API.py
+++ API.py
@@ -1,21 +1,27 @@
 import ReadSMS as r_sms
 import SendSMS as s_sms
 
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
 @app.route("/get_message", methods=["GET"])
 def get_message():
-    script = r_sms.read_sms() 
+    script = r_sms.read_sms()
     return script
 
 
-@app.route("/send_message", methods=["GET"])
+@app.route("/send_message", methods=["POST"])
 def send_message():
-    message = s_sms.send_sms("+5541987012013", "This is a test message")
-    return message
+    body_request = request.get_json()
 
+    destination = body_request["destination"]
+    message = body_request["message"]
+
+    message = s_sms.send_sms(destination, message)
+    return message
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
+
+
